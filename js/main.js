b5b1ecc06f1cc6f6f85464f85ef2686f3e94daed
Austin Song <austinhssong@gmail.com>
2024-03-06 11:15:25 -0500

    setup basic js for App, created clearSuggestions function

---
+++ js/main.js
@@ -0,0 +1,41 @@
+import { namesOne, namesTwo } from "./names";
+
+const initApp = () => {
+  document.getElementById("submitForm").addEventListener("submit", (e) => {
+    //prevent page refresh on form submit
+    e.preventDefault();
+    //clear out previous suggestions
+    clearSuggestions();
+    //generate names
+    //display names
+  });
+};
+
+document.addEventListener("DOMContentLoaded", initApp);
+
+const clearSuggestions = () => {
+  const display = document.getElementById("suggestionSection");
+  //add hidden if display not hidden
+  if (!display.classList.contains("hidden")) {
+    display.classList.toggle("hidden");
+  }
+  const list = document.querySelector(".suggestionSection ol");
+  list.innerHTML = "";
+};
+
+const generateNames = () => {
+  const randomNumArr = [];
+  for (let i = 0; i < 4; ) {
+    const randomNum = Math.floor(Math.random() * 10);
+    if (randomNumArr.includes(randomNum)) {
+      continue;
+    }
+    randomNumArr.push(randomNum);
+    i++;
+  }
+  const suggestion1 = namesOne[randomNumArr[0]] + namesTwo[randomNumArr[3]]
+  const suggestion2 = namesOne[randomNumArr[1]] + namesTwo[randomNumArr[0]]
+  const suggestion3 = namesOne[randomNumArr[2]] + namesTwo[randomNumArr[2]]
+  const suggestion4 = namesOne[randomNumArr[3]] + namesTwo[randomNumArr[1]]
+  
+};
