2aab2743c2d40245c15d1277fe34ed20e165b7f5
Austin Song <austinhssong@gmail.com>
2024-03-07 12:10:09 -0500

    display names func, need to redo innerHtml to clean

--- js/main.js
+++ js/main.js
@@ -7,8 +7,8 @@ const initApp = () => {
     //clear out previous suggestions
     clearSuggestions();
     //generate names
-    const namesArray = generateNames()
-    console.log(namesArray)
+    const namesArray = generateNames();
+    console.log(namesArray);
     //display names
   });
 };
@@ -35,11 +35,24 @@ const generateNames = () => {
     randomNumArr.push(randomNum);
     i++;
   }
-  console.log(randomNumArr)
-  const suggestion1 = namesOne[randomNumArr[0]] + namesTwo[randomNumArr[3]]
-  const suggestion2 = namesOne[randomNumArr[1]] + namesTwo[randomNumArr[0]]
-  const suggestion3 = namesOne[randomNumArr[2]] + namesTwo[randomNumArr[2]]
-  const suggestion4 = namesOne[randomNumArr[3]] + namesTwo[randomNumArr[1]]
+  console.log(randomNumArr);
+  const suggestion1 = namesOne[randomNumArr[0]] + namesTwo[randomNumArr[3]];
+  const suggestion2 = namesOne[randomNumArr[1]] + namesTwo[randomNumArr[0]];
+  const suggestion3 = namesOne[randomNumArr[2]] + namesTwo[randomNumArr[2]];
+  const suggestion4 = namesOne[randomNumArr[3]] + namesTwo[randomNumArr[1]];
 
-  return [suggestion1, suggestion2, suggestion3, suggestion4]
+  return [suggestion1, suggestion2, suggestion3, suggestion4];
+};
+
+const displayNames = (namesArray) => {
+  const list = document.querySelector(".suggestionSection ol");
+  const firstName = document.getElementById("submitSection__textInput").value;
+  namesArray.forEach((name) => {
+    list.innerHTML += `<li>
+    <a href="https://youtube.com/${name}" target="_blank">${name}</a></li>`;
+    list.innerHTML += `<ul>
+    <li><a href="<a href="https://youtube.com/${firstName}s${name}" target="_blank">${firstName}s${name}</a></li>
+    <li><a href="<a href="https://youtube.com/${name}With${firstName}" target="_blank">${firstName}s${name}</a></li>
+    `
+  });
 };
